13d41a6dd10f1a8a761cbbcff616519a96ef9f7c
taejun0 <xownswns@naver.com>
2024-07-16 04:00:25 +0900

    2nd HW finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,88 @@
-import { styled, ThemeProvider } from "styled-components";
+import { styled, ThemeProvider, createGlobalStyle } from "styled-components";
 
 import { Outlet } from "react-router-dom";
 
-const Wrapper = styled.div`
-  width: 100vw;
-  background-color: white;
+import { HiSearch } from "react-icons/hi";
 
-  * {
-    border: 1px solid black;
-  }
+import { CgBell } from "react-icons/cg";
+
+// Header
+const Header = styled.div`
+  padding: 12px 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const Layout = () => {
+const Header_Left = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Header_Right = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Iconset = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 24px;
+
+  width: 40px;
+  height: 40px;
+`;
+
+const Button = styled.div`
+  height: 2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  display: flex;
+  align-items: center;
+  margin-left: 0.5rem;
+
+  font-size: 1rem;
+  border-radius: 1rem;
+  border: none;
+  outline: none;
+
+  background-color: #212529;
+  color: #FFF;
+  font-weight: 700;
+`;
+
+const Topnav = () => {
   return (
     <>
-      <Wrapper>
-        <h3>오늘 세션도 화이팅</h3>
-        <Outlet />
-      </Wrapper>
+      <Header>
+        <Header_Left>
+          velog
+        </Header_Left>
+        <Header_Right>
+          <Iconset>
+            <CgBell />
+          </Iconset>
+          <Iconset>
+            <HiSearch />
+          </Iconset>
+          <Button>로그인</Button>
+        </Header_Right>
+      </Header>
     </>
-  );
-};
+  )
+}
 
 function App() {
   return (
     <>
-      <Layout />
+      <Topnav />
+      <Outlet />
     </>
   );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import HomePage from "./pages/HomePage";
+import PostDetailPage from "./pages/PostDetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <HomePage /> }],
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path:"/detail/:postID", element: <PostDetailPage />}
+    ],
   },
 ]);
 
